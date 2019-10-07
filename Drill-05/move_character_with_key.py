618f42f3536c86fd14328566d84be7bf03448c16
2DGP-Yum <yumtr@naver.com>
2019-10-07 19:51:55 +0900

    움직이는거 완료

--- Drill-05/move_character_with_key.py
+++ Drill-05/move_character_with_key.py
@@ -4,61 +4,67 @@ KPU_WIDTH, KPU_HEIGHT = 800, 600
 
 def handle_events():
     global running
-    global dir
     global C_x, C_y
+    global p1, p2
+    global x,y, i
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                dir += 1
-            elif event.key == SDLK_LEFT:
-                dir -= 1
-            elif event.key == SDLK_ESCAPE:
-                running = False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                dir -= 1
-            elif event.key == SDLK_LEFT:
-                dir += 1
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                i = 0
+                p1 = (x, y)
+                p2 = event.x, event.y
+                if p2[0] - p1[0] < 0:
+                    is_right = True
+                else:
+                    is_right = False
         elif event.type == SDL_MOUSEMOTION:
             C_x, C_y = event.x + 18, KPU_HEIGHT - 1 - event.y - 20
     pass
 
-def Move():
-    global dir
-    if x > 800:
-        dir -= 2
-    elif x < 0:
-        dir += 2
+def Move(p1, p2):
+    global x, y
+    global i
+    if i < 100:
+        t = i / 100
+        x = (1 - t) * p1[0] + t * p2[0]
+        y = (1 - t) * p1[1] + t * p2[1]
+        i = i + 3
+
+
+
+
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 KPU = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 hand_curser = load_image('hand_arrow.png')
 running = True
-x = 800 // 2
+x = 400
+y = 300
 frame = 0
 dir = 1
-C_x, C_y = 0,0
-
-
+C_x, C_y = 0, 0
+i = 0
+p1, p2 = (x, y), (x, y)
+is_right = True
 while running:
     clear_canvas()
     KPU.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     hand_curser.draw(C_x, C_y)
-    if dir < 0:
-        character.clip_draw(frame * 100, 0 * 1, 100, 100, x, 90)
-    else:
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
-    update_canvas()
 
+    Move(p1, p2)
+    if is_right == True:
+        character.clip_draw(frame * 100, 0 * 1, 100, 100, x, KPU_HEIGHT - 1 - y)
+    else:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, KPU_HEIGHT - 1 - y)
+    update_canvas()
     handle_events()
-    Move()
     frame = (frame + 1) % 8
-    x += dir * 10
+
     delay(0.05)
 
 close_canvas()
