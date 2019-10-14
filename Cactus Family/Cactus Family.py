0b3e0bec234a713d3d18ac34534d49cafe0759ea
2DGP-Yum <yumtr@naver.com>
2019-10-15 02:00:13 +0900

    충돌 싹다갈음
    그룹으로 지정해서 설정
    하지만 2개의 그룹이 될경우 수정해야함

--- Cactus Family/Cactus Family.py
+++ Cactus Family/Cactus Family.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from pico2d import *
+import random
 
 MAP_WIDTH = 900
 MAP_HEIGHT = 800
@@ -56,7 +57,6 @@ class Stone(Player):
                 self.ydir = self.ST_Y_NONE
 
     def handle_Stone(self, event):
-        # if 0 < test.y < MAP_HEIGHT and 0 < test.x - 50 < MAP_WIDTH:
         if event.type == SDL_KEYDOWN:
             if self.xdir == self.ST_X_NONE and self.ydir == self.ST_Y_NONE:
                 if event.key == SDLK_d and self.rect[2] < MAP_WIDTH - 50:
@@ -84,161 +84,115 @@ class Stone(Player):
 
 class Cactus(Stone):
     global player
+    global coll_group
 
     def __init__(self, px, py):
         self.xdir = self.ST_X_NONE
         self.ydir = self.ST_Y_NONE
-        self.set_pos(px, py)
+        # self.set_pos(px, py)
+        self.x = random.randint(1, 8) * 100
+        self.y = random.randint(1, 7) * 100
         self.old_x, self.old_y = 0, 0
         self.frame = 0
-        self.speed = 25
-        self.left = 50
-        self.top = 50
-        self.right = 50
-        self.bottom = 50
-        self.rect = [self.x - self.left, self.y + self.top, self.x + self.right, self.y - self.bottom]
-        self.state = 0
-        self.left_col = False
-        self.top_col = False
-        self.right_col = False
-        self.bottom_col = False
-        self.isMerge = False
-        self.isParents = False
-        self.nx = 0
-        self.ny = 0
-        self.P = 0
-
-    def Set_NewPos(self, ano):
-        if 1:
-            self.old_x = self.x
-            self.old_y = self.y
-            self.nx = self.old_x
-            self.ny = self.old_y
-
-            print('뭘봐')
-            print(self.old_x, self.nx, self.x)
-
-            if self.old_x < ano.x:
-                self.nx = ano.x - self.old_x
-            else:
-                self.nx = self.old_x - ano.x
-            if self.old_y < ano.y:
-                self.ny = ano.y - self.old_y
-            else:
-                self.ny = self.old_y - ano.y
-            self.isMerge = True
+        self.speed = 20
+        self.rect = [self.x - 50, self.y + 50, self.x + 50, self.y - 50]
+        self.isColl = False
 
     def New_coll(self, ano):
-        if self.isMerge:
-            self.x = ano.x - self.nx
-            self.y = ano.y - self.ny
-
-        if not self.isParents:
-            if self.rect[1] == ano.rect[1] and self.rect[3] == ano.rect[3]:
-                # 오른쪽 닫았다
-                if self.rect[2] == ano.rect[0]:
-                    self.Set_NewPos(ano)
-                    print('오른쪽')
-                # 왼쪽 닫았다
-                if self.rect[0] == ano.rect[2]:
-                    if not ano.isParents:
-                        self.isParents = True
-                    elif ano.isParents:
-                        self.Set_NewPos(ano)
-                    print('왼쪽')
-            elif self.rect[0] == ano.rect[0] and self.rect[2] == ano.rect[2]:
-                # 위쪽 닫았다
-                if self.rect[1] == ano.rect[3] and not self.top_col:
-                    self.top_col = True
-                    self.old_y = self.y + 100
-                # 아래쪽 닫았다
-                if self.rect[3] == ano.rect[1] and not self.bottom_col:
-                    self.bottom_col = True
-                    self.old_y = self.y - 100
-
-        # if self.xdir == self.ST_X_NONE and self.ydir == self.ST_Y_NONE:
-        #     if self.right_col:
-        #         self.xdir = ano.xdir
-        #         self.ydir = ano.ydir
-        #         print('좌 붙음')
-        #     if self.left_col:
-        #         self.xdir = ano.xdir
-        #         self.ydir = ano.ydir
-        #         print('우 붙음')
-
-            # if self.top_col:
-            #     #print('하 붙음')
-            #
-            # if self.bottom_col:
-            #     #print('하 붙음')
+        if 1:
+            # 왼쪽 닫았다
+            if self.rect[0] == ano.rect[2] and self.y == ano.y:
+                self.isColl = True
+                ano.isColl = True
+                # print('왼쪽')
+            # 위쪽 닫았다
+            elif self.rect[1] == ano.rect[3] and self.x == ano.x:
+                self.isColl = True
+                ano.isColl = True
+                # print('위쪽')
 
     def collision(self):
         if self.rect[3] == player.rect[3] and player.rect[1] == self.rect[1] and player.xdir != player.ST_X_NONE:
             # 오른쪽 충돌
             if self.rect[2] >= player.rect[0] and self.rect[0] < player.rect[2] and player.xdir == player.ST_X_BAKWARD:
-                #if self.isMerge:
-                self.xdir = self.ST_X_BAKWARD
-                #self.x -= self.speed
-                self.old_x = self.x
+                if self.isColl:
+                    for i in coll_group:
+                        cac[i].xdir = cac[i].ST_X_BAKWARD
+                        cac[i].old_x = cac[i].x - 25
+                else:
+                    self.xdir = self.ST_X_BAKWARD
+                    self.old_x = self.x - 25
                 print('col 0')
             # 왼쪽 충돌
             elif self.rect[0] <= player.rect[2] and self.rect[2] > player.rect[0] and player.xdir == player.ST_X_FORWARD:
-                self.xdir = self.ST_X_FORWARD
-                #self.x += self.speed
-                self.old_x = self.x
+                if self.isColl:
+                    for i in coll_group:
+                        cac[i].xdir = cac[i].ST_X_FORWARD
+                        cac[i].old_x = cac[i].x + 25
+                else:
+                    self.xdir = self.ST_X_FORWARD
+                    self.old_x = self.x + 25
                 print('col 1')
-            else:
-                self.xdir = self.ST_X_NONE
+
         elif self.rect[0] == player.rect[0] and player.rect[2] == self.rect[2] and player.ydir != player.ST_Y_NONE:
             # 위 충돌
             if self.rect[1] >= player.rect[3] and self.rect[3] < player.rect[1] and player.ydir == player.ST_Y_DOWN:
-                self.ydir = self.ST_Y_DOWN
-                #self.y -= self.speed
-                self.old_y = self.y
+                if self.isColl:
+                    for i in coll_group:
+                        cac[i].ydir = cac[i].ST_Y_DOWN
+                        cac[i].old_y = cac[i].y - 25
+                else:
+                    self.ydir = self.ST_Y_DOWN
+                    self.old_y = self.y - 25
+
                 print('col 2')
             # 아래 충돌
             elif self.rect[3] <= player.rect[1] and self.rect[1] > player.rect[3] and player.ydir == player.ST_Y_UP:
-                self.ydir = self.ST_Y_UP
-                #self.y += self.speed
-                self.old_y = self.y
-                print('col 3')
-            else:
-                self.ydir = self.ST_X_NONE
+                if self.isColl:
+                    for i in coll_group:
+                        cac[i].ydir = cac[i].ST_Y_UP
+                        cac[i].old_y = cac[i].y + 25
+                else:
+                    self.ydir = self.ST_Y_UP
+                    self.old_y = self.y + 25
 
-    def collision2(self, ano):
-        # 좌우
-        if self.rect[3] == ano.rect[3] and ano.rect[1] == self.rect[1]:
-            if self.rect[2] == ano.rect[0] and ano.state:
-                # self.x = ano.x - (ano.left + ano.right)
-                self.right += ano.right + ano.left
-                ano.state = 0
-                print('1')
-            elif self.rect[0] == ano.rect[2] and not ano.state:
-                # self.x = ano.x + (ano.left + ano.right)
-                self.left += ano.right + ano.left
-                ano.state = 1
-                print('2')
-        # 상하
-        elif self.rect[0] == ano.rect[0] and ano.rect[2] == self.rect[2]:
-            if self.rect[1] == ano.rect[3]:
-                # self.y = ano.y - (ano.top + ano.bottom)
-                self.top += ano.top + ano.bottom
-                print('3')
-            elif self.rect[3] == ano.rect[1]:
-                # self.y = ano.y + (ano.top + ano.bottom)
-                self.bottom += ano.top + ano.bottom
-                print('4')
+                print('col 3')
+
+    def handle_cactus(self, event):
+        if event.type == SDL_KEYDOWN:
+            if self.xdir == self.ST_X_FORWARD and event.key == SDLK_d:
+                print('hi')
+                if self.isColl:
+                    for i in coll_group:
+                        cac[i].old_x = cac[i].x + 100
+                        print('hi')
+                else:
+                    self.old_x = self.x + 100
+            elif self.xdir == self.ST_X_BAKWARD and event.key == SDLK_a:
+                if self.isColl:
+                    for i in coll_group:
+                        cac[i].old_x = cac[i].x - 100
+                else:
+                    self.old_x = self.x - 100
+            elif self.ydir == self.ST_Y_UP and event.key == SDLK_w:
+                if self.isColl:
+                    for i in coll_group:
+                        cac[i].old_y = cac[i].y + 100
+                else:
+                    self.old_y = self.y + 100
+            elif self.ydir == self.ST_Y_DOWN and event.key == SDLK_s:
+                if self.isColl:
+                    for i in coll_group:
+                        cac[i].old_y = cac[i].y - 100
+                else:
+                    self.old_y = self.y - 100
 
     def render(self):
         self.draw_image(8, 100, 100)
 
     def update(self):
         self.move()
-        # if self.isMerge:
-        #     print(self.old_x, self.nx, self.x)
-        #     self.rect = [self.nx - 50, self.ny + 50, self.nx + 50, self.ny - 50]
-        # else:
-        self.rect = [self.x - self.left, self.y + self.top, self.x + self.right, self.y - self.bottom]
+        self.rect = [self.x - 50, self.y + 50, self.x + 50, self.y - 50]
 
 
 def exit():
@@ -248,7 +202,7 @@ def exit():
 
 
 def handle_events():
-    global running, cac_count
+    global running, cac_count, coll_group
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -259,18 +213,20 @@ def handle_events():
             elif event.key == SDLK_q:
                 for i in range(0, cac_count):
                     cac[i].__init__((i * 100) + 200, (i * 100) + 200)
-                    cac[i].state = 0
+                    coll_group.clear()
             elif event.key == SDLK_e:
                 print(player.rect)
-                for i in range(0, cac_count):
+                for i in range(cac_count):
                     print(cac[i].rect)
             else:
                 player.handle_Stone(event)
+                for i in range(cac_count):
+                    cac[i].handle_cactus(event)
         elif event.type == SDL_KEYUP:
             player.handle_Stone(event)
 
 
-cac_count = 2
+cac_count = 10
 
 open_canvas(MAP_WIDTH, MAP_HEIGHT)
 player = Stone()
@@ -281,7 +237,22 @@ for i in range(cac_count):
     cac[i].set_image('Cactus test.png')
 Map_Test = load_image('Map_1.png')
 running = True
-BGM = load_music('SweetPoop .mp3')
+coll_group = []
+
+
+def make_group():
+    global coll_group
+    for j in range(cac_count):
+        if cac[j].isColl:
+            coll_group.append(j)
+            # print(j)
+    coll_group = list(set(coll_group))
+
+
+def test():
+    global coll_group
+    # print(coll_group)
+    pass
 
 
 def render():
@@ -291,6 +262,9 @@ def render():
     for i in range(0, cac_count):
         draw_rectangle(cac[i].rect[0], cac[i].rect[1], cac[i].rect[2], cac[i].rect[3])
         cac[i].render()
+    make_group()
+    test()
+
 
 
 def update():
