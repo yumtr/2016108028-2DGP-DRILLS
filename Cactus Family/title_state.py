71ec54d9f8f2f6213dccb3cd2f91013028ef825d
2DGP-Yum <yumtr@naver.com>
2019-11-15 01:34:54 +0900

    타이틀 메뉴바 규현
    규현은 슈퍼주니어의 멤버이다.

--- Cactus Family/title_state.py
+++ Cactus Family/title_state.py
@@ -2,36 +2,65 @@ import game_framework
 import Cactus_Family
 from pico2d import *
 
-
 name = "TitleState"
 image = None
+menu_image = None
+arrow = None
+arrow_position = 350
+menu_num = 1
+
+
+def choose_menu():
+    if menu_num == 1:
+        game_framework.change_state(Cactus_Family)
+    elif menu_num == 2:
+        pass
+    elif menu_num == 3:
+        game_framework.quit()
+
+
+def move_arrow(y_pos):
+    global arrow_position, menu_num
+    if y_pos == 0 and menu_num > 1:
+        arrow_position += 100
+        menu_num -= 1
+    elif y_pos == 1 and menu_num < 3:
+        arrow_position -= 100
+        menu_num += 1
 
 
 def enter():
-    global image
+    global image, menu_image, arrow
     image = load_image('image_file\\title.png')
+    menu_image = load_image('image_file\\menu.png')
+    arrow = load_image('image_file\\arrow.png')
 
 
 def exit():
-    global image
-    del image
+    global image, menu_image, arrow
+    del image, menu_image, arrow
 
 
 def handle_events():
     events = get_events()
     for event in events:
-        if event.type == SDL_QUIT:
-            game_framework.quit()
-        else:
-            if(event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+        if event.type == SDL_KEYDOWN:
+            if (event.key) == (SDLK_ESCAPE):
                 game_framework.quit()
-            elif(event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-                game_framework.change_state(Cactus_Family)
-
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_UP):
+                move_arrow(0)
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_DOWN):
+                move_arrow(1)
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_RETURN):
+                choose_menu()
+        elif event.type == SDL_QUIT:
+            game_framework.quit()
 
 def draw():
     clear_canvas()
     image.draw(450, 400)
+    menu_image.draw(450, 250)
+    arrow.draw(240, arrow_position)
     update_canvas()
 
 
@@ -45,9 +74,3 @@ def pause():
 
 def resume():
     pass
-
-
-
-
-
-
